0533204df24e1cf7c6bb391ae28456c2e332048c
AR0406 <arfaz0604@gmail.com>
2025-09-09 12:40:33 +0530

    Create q2.c

---
+++ q2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a=10;  //first number 
+    int b =5;  //second number 
+    int sum;   //variable to store result
+
+    sum =a+b ;  // add two numbers
+    printf ("The sum is: %d , sum");
+    return 0 ;
+}
